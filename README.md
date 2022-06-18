ac3774e76ea84652d517f0ee03f9ab35e5eaf5df
StanGirard <stanislasg@padok.fr>
2022-06-18 12:50:27 +0200

    feat: 🎸 readme
    added code

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# argocd-diff-finder
-Bash script to find the diff between your latest commit and what is deployed via argued
+<p align="center"><img src="https://argo-cd.readthedocs.io/en/stable/assets/logo.png" width="180px" /></p>
+
+
+# Argocd Diff Finder
+
+**Argocd Diff Finder** is a small bash script that can tell you what your actual changes on your resources will be when you merge your changes to the ArgoCD repository.
+
+## Why you need it
+
+- It's **free**, easy and open source. 
+- It's easy to install
+- No more pain knowing what your changes will be when you merge your changes to the ArgoCD repository.
+
+## Features
+
+- It's a bash script :D (no need to install anything)
+- None except telling you the differences between your changes and the state of your app in Kubernetes.
+
+## Demo
+
+Display the demo.gif from folder demo
+
+<p align="center"><img src="../argocd-diff-finder/demo/demo.gif" width="800px" /></p>
+
+## How it works
+
+Argocd Diff Finder works with two configurations:
+- All your app declarations and their helm charts, yaml files are in the same repository (ARGOCD_GIT_URL)
+- You have a repository for your app declarations and point to another repository for your helm charts and yaml files.
+  - In this case, Argocd diff finder needs to be run on the repository that host your helm charts and yaml files.
+  - It will clone the argocd repository and then run the diff finder on the cloned repository.
+    - It will extract information in your app configurations in order to find the name of your argo apps that have been impacted by the changes
+- It uses the `argocd app diff` command to find the differences between your changes and the state of your app in Kubernetes. 
+
+## Usage
+
+- Optional: Install dyff
+  - `brew install homeport/tap/dyff`
+  - `export KUBECTL_EXTERNAL_DIFF="dyff between -b "`
+- Go to your branch with the changes you want to merge.
+- Make sure you pushed your changes
+- Make a port forward to your argocd server if it is not publicly accessible
+- Export the following variables
+```bash
+export ARGOCD_SERVER=<localhost:8080> # The ArgoCD server address
+export ARGOCD_USERNAME=<username> # The ArgoCD username
+export ARGOCD_PASSWORD=<password> # The ArgoCD password
+export ARGOCD_GIT_URL=<git-url> # The ArgoCD git url with you applications configuration
+```
+- Optional: change some values
+```bash
+export MAIN_BRANCH=<main-branch> # The main branch of your application - default main
+export ARGOCD_TMP_DIR=<tmp-dir> # The temporary directory to store the diffs - default /tmp/argocd-cd-tmp
+```
+
+- Run the script on your branch with the changes
+```bash
+bash argocd_diff_finder.sh #./argocd_diff_finder.sh
+```
+
+## Contributions
+
+Please feel free to add any contribution.
+I might, if I have the time, translate it into go.
+
+## Disclaimers
+
+It comes as is, without any warranty.
---
+++ argocd_diff_finder.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+if [[ -z ${ARGOCD_SERVER} ]]; then
+  echo "ARGOCD_SERVER is not set"
+  exit 1
+fi
+if [[ -z "${ARGOCD_USERNAME}" ]]; then
+  echo "ARGOCD_USERNAME is not set"
+  exit 1
+fi
+
+if [[ -z "${ARGOCD_PASSWORD}" ]]; then
+  echo "ARGOCD_USERNAME is not set"
+  exit 1
+fi
+
+if [[ -z "${ARGOCD_GIT_URL}" ]]; then
+  echo "ARGOCD_GIT_URL is not set"
+  exit 1
+fi
+
+
+## Git find default branch
+MAIN_BRANCH_REPO=${MAIN_BRANCH:-"main"}
+latestCommitHash=$(git rev-parse HEAD)
+ARGOCD_TMP_DIR=${TMP_DIR:-"/tmp/argocd-cd-tmp"}
+ACTUALGITREPOURL=$(git config --get remote.origin.url)
+PWD=$(pwd)
+
+cecho(){
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"  
+    YELLOW="\033[0;33m"
+    CYAN="\033[0;36m"
+    BLUE="\033[0;34m"
+    PURPLE="\033[0;35m"
+    WHITE="\033[0;37m"
+    BOLD="\033[1;1m"
+    # ... Add more colors if you like
+
+    NC="\033[0m" # No Color
+
+    # printf "${(P)1}${2} ${NC}\n" # <-- zsh
+    printf "${!1}${2} ${NC}${3}\n" # <-- bash
+}
+
+information () {
+    cecho "GREEN" "$1"
+}
+bold () {
+    cecho "BOLD" "$1" 
+}
+
+warning () {
+    cecho "RED" "$1"
+}
+
+loginArgocd() {
+    information "Login to ArgoCD ... 🌹"
+    argocd logout $ARGOCD_SERVER
+    yes | argocd login --username $ARGOCD_USERNAME --password $ARGOCD_PASSWORD $ARGOCD_SERVER
+}
+
+logoutArgocd() {
+    information "Logout from ArgoCD ... 🌹"
+    argocd logout $ARGOCD_SERVER
+}
+
+argocdGitUrlDiff() {
+    if [ "$ACTUALGITREPOURL" != "$ARGOCD_GIT_URL" ]; then
+        echo 1
+    ## Else
+    else
+        echo 0
+    ## Done
+    fi
+}
+
+findingAllFilesChangedFromMainBranch() {
+    filesChanged=$(git diff --name-only --diff-filter=AM $MAIN_BRANCH_REPO | grep -E "\.ya?ml$")
+    if [ -z "$filesChanged" ]; then
+        exit 0
+    else
+        ## Return the files changed
+        echo $filesChanged
+    fi
+}
+
+argocdAppDiff() {
+    
+    filesYAML=$(find .   -type f -name "*.yaml" -o -type f -name "*.yml")
+    ## Echo the length of the filesYAML
+    for file in $filesYAML; do
+        if grep -q "{{ " $file; then
+            continue
+        fi
+        name=$(yq -e .metadata.name $file  | sed 's/^"//' | sed 's/"$//' | uniq )
+        path=$(yq -e .spec.source.path $file 2> /dev/null | sed 's/^"//' | sed 's/"$//' | uniq)
+        giturlYaml=$(yq -e .spec.source.repoURL $file 2> /dev/null | sed 's/^"//' | sed 's/"$//' | uniq)
+        if [ -n "$name" ] && [ -n "$path" ] && [ "$giturlYaml" == $ACTUALGITREPOURL ]; then
+            ## If the path is in filesChanged, then print the name
+            if [[ $1 =~ $path ]]; then
+                echo "Name: $name"
+                echo "Path: $path"
+                echo "File Changed: $file"
+                echo "---> Running Diff"
+                argocd app diff $name --revision $latestCommitHash
+                echo "----> Diff Complete"
+            fi
+        fi
+    done
+}
+
+cloneArgocdCDRepository() {
+    information "Clone ArgoCD CD Repository ... 🌹"
+    ## Clone in temporary directory
+    rm -rf  $ARGOCD_TMP_DIR
+    git clone $ARGOCD_GIT_URL $ARGOCD_TMP_DIR
+    information "Finished cloning ArgoCD CD Repository ... 🌹"
+}
+
+argocdDiff() {
+    echo "Checking if in ArgoCD Repository"
+    repoTrue=$(argocdGitUrlDiff)
+    loginArgocd
+    if [ $repoTrue -eq 1 ]; then
+        warning "Not in ArgoCD Root Repository"
+        filesChanged=$(findingAllFilesChangedFromMainBranch)
+        if [ -z $filesChanged ]; then
+            warning "No files changed"
+            exit 0
+        fi
+        information "Files Changed: "
+        echo $filesChanged
+        cloneArgocdCDRepository
+        cd $ARGOCD_TMP_DIR
+        argocdAppDiff $filesChanged 
+        exit 0
+    else
+        echo "In ArgoCD Repository"
+        ## Return the files changed
+        filesChanged=$(findingAllFilesChangedFromMainBranch)
+        if [ -z "$filesChanged" ]; then
+            warning "No files changed"
+            exit 0
+        fi
+        information "Files Changed: "
+        echo $filesChanged
+        argocdAppDiff $filesChanged 
+
+    fi
+    logoutArgocd
+}
+
+information "Main Branch is ${MAIN_BRANCH_REPO}"
+
+
+argocdDiff
